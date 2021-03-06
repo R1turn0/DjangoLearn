94283bac1e81b6c56cab7357dadf6a64f3058c92
AyatukiRin <zrl02081653@gmail.com>
2021-03-06 19:06:05 +0800

    我真的吐了，就因为少了一个/！！！！！！

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'polls'  # 设置命名空间
+app_name = "polls"  # 设置命名空间
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/', views.detaild, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
